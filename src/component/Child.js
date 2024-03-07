a460addc0390d348873541a61c1d1b2735c9bcf7
kyh012412 <kyh0124.12@gmail.com>
2024-03-07 14:56:28 +0900

    정상적인 React.memo사용 goodcase

--- src/component/Child.js
+++ src/component/Child.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 const Child = ({ name, age }) => {
   console.log('자녀 컴포넌트 렌더링!');
@@ -17,4 +17,7 @@ const Child = ({ name, age }) => {
   );
 };
 
-export default Child;
+export default memo(Child);
+//최적화 하고싶은 부분을 memo()로 감싼다.
+//렌더링이 될 상황에 놓일 때마다 prop체크를 하게된다.
+//props에 변화가 없다면 렌더링을 하지 않는다.
